3c3ccffe05cc4f6b8d1cf8ecfabffef34485bdcd
carapaceonbear <seerhan17@gmail.com>
2022-06-15 16:43:27 +0100

    initial styling for pop out

--- src/components/PopOut/PopOut.jsx
+++ src/components/PopOut/PopOut.jsx
@@ -4,26 +4,42 @@ import "./PopOut.scss"
 const PopOut = ({beer, onClick}) => {
 
   if (beer != null) {
-    const {name, image_url, tagline, first_brewed, description, abv, ph, ingredients, hops, food_pairing, brewers_tips} = beer;
+    const {name, image_url, tagline, first_brewed, description, abv, ph, food_pairing, brewers_tips} = beer;
+    
+    // const food_pairs = food_pairing.map((item, index) => {
+    //         if (index == 0) {
+    //             return {item};
+    //         } else {
+    //             return " , {item}"
+    //         }
+    //     }
+
     return (
-      <div className="pop-out" onClick={onClick}>
-        <i class="fa-solid fa-xmark fa-2xl"></i>
-        <h2 className="pop-out__name">{name}</h2>
-        <img className="pop-out__image" src={image_url} alt={name} />
-        <h3 className="pop-out__tagline">{tagline}</h3>
-        <div className="pop-out__stats">
-          <h4 className="pop-out__stat-text">{abv}</h4>
-          <h4 className="pop-out__stat-text">{ph}</h4>
-          <h4 className="pop-out__stat-text">{first_brewed}</h4>
+      <>
+        <div className="overlay"></div>
+        <div className="pop-out">
+            <i className="fa-solid fa-xmark fa-2xl pop-out__cross" onClick={onClick}></i>
+            <h2 className="pop-out__name">{name}</h2>
+            <section className="pop-out__top">
+                <img className="pop-out__image" src={image_url} alt={name} />
+                <div className="pop-out__stats">
+                    <h4 className="pop-out__stat-text">ABV : {abv}</h4>
+                    <h4 className="pop-out__stat-text">pH : {ph}</h4>
+                    <h4 className="pop-out__stat-text">First Brewed, {first_brewed}</h4>
+                </div>
+            </section>
+            <h3 className="pop-out__tagline">{tagline}</h3>
+            <section className="pop-out__bottom">
+                <div className="pop-out__description">
+                    <p className="pop-out__copy">{description}</p>
+                    <p className="pop-out__copy">{brewers_tips}</p>
+                </div>
+                <div className="pop-out__pairing-list">
+                    <p className="pop-out__copy">{food_pairing}</p>
+                </div>
+            </section>
         </div>
-        <div className="pop-out__description">
-          <p className="pop-out__desc--copy">{description}</p>
-          <p className="pop-out__desc--list">{ingredients[0]}</p>
-          <h4 className="pop-out__desc--aside">{food_pairing}</h4>
-          <p className="pop-out__desc--copy">{brewers_tips}</p>
-          <p className="pop-out__desc--list">{hops}</p>
-        </div>
-      </div>
+      </>
     )
   }
 }
